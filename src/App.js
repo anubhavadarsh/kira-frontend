9b4633a97122f898aa8ed54aad693b7831afd57b
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-06-26 14:16:14 +0530

    fix: improve style and hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import kira from "./assets/kira.png";
 import "./App.css";
 import AnswerBox from "./components/AnswerBox";
 import ChatBox from "./components/ChatBox";
@@ -9,6 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <div className="imgCont">
+        <img src={kira} width="150" height="150" className="img" />
+      </div>
       <ChatBox setAnswer={setAnswer} />
       {answer.length > 0 && <AnswerBox answer={answer} />}
     </div>
--- src/components/ChatBox/hook.js
+++ src/components/ChatBox/hook.js
@@ -1,5 +1,3 @@
-import config from "../../config";
-
 const useGetAnswer = () => {
   const getAnswer = (question, setAnswer) => {
     (async () => {
